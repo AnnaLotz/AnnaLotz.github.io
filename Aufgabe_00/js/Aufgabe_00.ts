efb6545d994493895c0a7375bce8069e0adbc288
Anna <Anna@Laptop-von-Anna.eduroam.hs-furtwangen.de>
2018-03-29 11:43:10 +0200

    Aufgabe 0 im userpage repo gelöscht

--- Aufgabe_00/js/Aufgabe_00.ts
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var name : string = prompt ("Wie ist dein Name?", "Name");
-    var container : HTMLElement = document.getElementById("hallo");
-        container.innerHTML = "Hallo " + name + ", moege die Macht mit dir sein!";
-        }
-    );
